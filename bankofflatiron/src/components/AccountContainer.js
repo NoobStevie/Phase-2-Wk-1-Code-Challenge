bace8df9c14ce0064b3cf6f0cb972bc9fdf99ef8
Steve <kamaustevek78@gmail.com>
2023-04-25 20:42:40 +0300

    Completed Assignment

--- bankofflatiron/src/components/AccountContainer.js
+++ bankofflatiron/src/components/AccountContainer.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 import TransactionsList from "./TransactionsList";
 
 function AccountContainer() {
+    const [transaction, setTransaction] = useState([])
+    const [query, setQuery] = useState("")
+    useEffect(() => {
+        fetch("http://localhost:3000/transactions" + query)
+        .then((resp) => resp.json())
+        .then(transaction => setTransaction(transaction))
+    }, [query])
+    function handleSearch(e) {
+        setQuery(e.target.value)
+    }
+
     return (
         <div>
-            <Search />
+            <Search handleSearch={handleSearch} />
             <AddTransactionForm />
-            <TransactionsList />
+            <TransactionsList transactions={transaction}/>
         </div>
     );
 }
--- bankofflatiron/src/components/Transaction.js
+++ bankofflatiron/src/components/Transaction.js
@@ -1,26 +1,13 @@
 import React from "react";
 
-function Transaction() {
+function Transaction({date, description, category, amount}) {
     return (
-        <table className="ui celled striped padded table">
-        <tbody>
-            <tr>
-                <th>
-                    <h3 className="ui center aligned header">Date</h3>
-                </th>
-                <th>
-                    <h3 className="ui center aligned header">Description</h3>
-                </th>
-                <th>
-                    <h3 className="ui center aligned header">Category</h3>
-                </th>
-                <th>
-                    <h3 className="ui center aligned header">Amount</h3>
-                </th>
-            </tr>
-            
-        </tbody>
-    </table>
+        <tr>
+            <td>{date}</td>
+            <td>{description}</td>
+            <td>{category}</td>
+            <td>{amount}</td>
+        </tr>
     );
 }
 

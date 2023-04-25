8e463bf08274dadc4fd0dce99f8494165df3f918
Steve <kamaustevek78@gmail.com>
2023-04-25 19:16:09 +0300

    Done

--- bankofflatiron/src/components/AccountContainer.js
+++ bankofflatiron/src/components/AccountContainer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import TransactionList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+import TransactionsList from "./TransactionsList";
 
 function AccountContainer() {
     return (
         <div>
             <Search />
             <AddTransactionForm />
-            <TransactionList />
+            <TransactionsList />
         </div>
     );
 }
